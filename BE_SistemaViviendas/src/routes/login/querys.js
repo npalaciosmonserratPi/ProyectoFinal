b1c171fd235639620fd5034b33ec278c36ef1504
Tamara Soria <tamarasoria35@outlook.com.ar>
2021-05-26 21:14:47 -0300

    Correccion en Query de login

--- BE_SistemaViviendas/src/routes/login/querys.js
+++ BE_SistemaViviendas/src/routes/login/querys.js
@@ -1,5 +1,5 @@
 function queryLogin(userName, password) {
-    return query = `SELECT * FROM Usuario WHERE userName=? and password=?`, [userName,password];
+    return `SELECT * FROM Usuario WHERE nombre='${userName}' and pass='${password}'`;
 }
 
 module.exports = {
